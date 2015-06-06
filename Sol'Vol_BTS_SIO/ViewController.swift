5b2ca21f6ac71d6b107c7a83185788366bcef060
Morad Bourial <Morad@MacBook-Pro-de-Morad.local>
2015-06-06 12:35:32 +0200

    Initial Commit

---
+++ Sol'Vol_BTS_SIO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sol'Vol_BTS_SIO
+//
+//  Created by Morad Bourial on 06/06/2015.
+//  Copyright (c) 2015 Morad Bourial. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
